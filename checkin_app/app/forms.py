53e2512d5f9c2780a9c5db51c9dab2939f816c58
I-Like-Me <cassgolkin@gmail.com>
2024-01-12 07:55:19 -0500

    auto focus added

--- checkin_app/app/forms.py
+++ checkin_app/app/forms.py
@@ -3,5 +3,5 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 class card_form(FlaskForm):
-    card_data = StringField('Card Data', validators=[DataRequired()])
+    card_data = StringField('Card Data', validators=[DataRequired()], render_kw={'autofocus': True})
     submit = SubmitField('Submit')
